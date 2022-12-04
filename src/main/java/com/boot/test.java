267077de48b70440802a876da07d84cfd34d3bbb
ubing-gx <73573917+ubing-gx@users.noreply.github.com>
2022-12-04 13:57:33 +0800

    Add files via upload

---
+++ src/main/java/com/boot/test.java
@@ -0,0 +1,16 @@
+package com.boot;
+
+import com.boot.JDBC.MySql;
+
+/**
+ * @ClassName : test
+ * @Description : test
+ * @Author : ubing
+ * @Date: 2022-12-04 12:28
+ */
+public class test {
+    public static void main(String[] args) {
+            MySql mySql = new MySql();
+            mySql.initCode();
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,16 @@
+import com.boot.JDBC.MySql;
+
+
+/**
+ * @ClassName : test
+ * @Description : test
+ * @Author : ubing
+ * @Date: 2022-12-02 10:14
+ */
+public class test {
+    public static void main(String[] args) {
+        MySql mySql = new MySql();
+        mySql.initCode();
+    }
+}
+
